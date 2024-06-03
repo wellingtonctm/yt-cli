d6e57c9637751d48ba2d7aa58db6689b2e627eb4
wellingtonctm <w.luiscm@gmail.com>
2024-06-03 18:20:18 -0300

    feature(xmobar): add status (PAUSED or PLAYING)

--- xmobar/yt-cli.sh
+++ xmobar/yt-cli.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-FILE='.config/yt-cli/song.info'
+SONG_INFO='.config/yt-cli/song.info'
+SONG_STATUS='.config/yt-cli/song.status'
 MAX=50
 
-if [[ -f "$FILE" ]]; then
-    TITLE=$(sed ':a;N;$!ba;s/\n/ - /g' "$FILE")
+if [[ -f "$SONG_INFO" ]]; then
+    [[ -f $SONG_STATUS ]] && [[ "$(cat $SONG_STATUS)" == 'PAUSED' ]] && STATUS='[PAUSED] '
+    TITLE="${STATUS}$(sed ':a;N;$!ba;s/\n/ - /g' "$SONG_INFO")"
 
     if [[ $(echo -n "$TITLE" | wc -m) -le $MAX ]]; then
         echo -n "${TITLE}"
