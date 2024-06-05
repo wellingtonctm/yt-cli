0cdd3df683edfb0e218c62338f1c399507c15584
wellingtonctm <w.luiscm@gmail.com>
2024-06-05 17:09:14 -0300

    refactor: using socket to check status of the song

--- xmobar/yt-cli.sh
+++ xmobar/yt-cli.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+SOCKET='.config/yt-cli/song.socket'
 SONG_INFO='.config/yt-cli/song.info'
 SONG_STATUS='.config/yt-cli/song.status'
 MAX=50
 
-if [[ -f "$SONG_INFO" ]]; then
-    [[ -f $SONG_STATUS ]] && [[ "$(cat $SONG_STATUS)" == 'PAUSED' ]] && STATUS='[PAUSED] '
-    TITLE="${STATUS}$(sed ':a;N;$!ba;s/\n/ - /g' "$SONG_INFO")"
+function is_paused() {
+    data=$(echo '{ "command": ["get_property", "pause"] }' | socat - "$SOCKET" | jq -r .data)
+
+    if [[ "$data" == "true" ]]; then
+        return 0
+    fi
+
+    return 1
+}
+
+if [[ -S "$SOCKET" ]]; then
+    TITLE=$(head -n 1 "$SONG_INFO")
+
+    is_paused && TITLE="[$TITLE]"
 
     if [[ $(echo -n "$TITLE" | wc -m) -le $MAX ]]; then
         echo -n "${TITLE}"
