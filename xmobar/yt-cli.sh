d23907dd6c0b449e6f1d9f381b2ba5bc087d5b44
wellingtonctm <w.luiscm@gmail.com>
2024-08-29 08:16:32 -0300

    refactor: Ajustes no desing do título e simbolo a serem exibidos

--- xmobar/yt-cli.sh
+++ xmobar/yt-cli.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
+PID='.config/yt-cli/song.pid'
 SOCKET='.config/yt-cli/song.socket'
 SONG_INFO='.config/yt-cli/song.info'
 SONG_STATUS='.config/yt-cli/song.status'
 MAX=50
 
 function is_paused() {
+    local data
     data=$(echo '{ "command": ["get_property", "pause"] }' | socat - "$SOCKET" | jq -r .data)
-
-    if [[ "$data" == "true" ]]; then
-        return 0
-    fi
-
-    return 1
+    [[ "$data" == "true" ]]
 }
 
-if [[ -S "$SOCKET" ]]; then
-    TITLE=$(head -n 1 "$SONG_INFO")
+if [[ -f "$PID" ]] && kill -0 $(cat "$PID") &> /dev/null; then
+    echo -n " "
+    
+    IFS= read -r TITLE < "$SONG_INFO"
 
-    is_paused && TITLE="[$TITLE]"
-
-    if [[ $(echo -n "$TITLE" | wc -m) -le $MAX ]]; then
-        echo -n "${TITLE}"
-    else
-        echo -n "$TITLE" | head -c $((MAX - 3))
-        echo -n "..."
+    if is_paused; then
+        TITLE="[$TITLE]"
     fi
 
-    echo " |"
+    if [[ ${#TITLE} -le $MAX ]]; then
+        echo -n "$TITLE"
+    else
+        echo -n "${TITLE:0:MAX-3}..."
+    fi
+
+    echo " <fc=#000000> | </fc>"
 fi
 
